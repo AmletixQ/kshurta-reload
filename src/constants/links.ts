3cda3fe524e69d9426f1636920d807fa42f9bb0b
AmletixQ <nazarovamridin308@gmail.com>
2025-11-12 21:48:44 +0300

    Создал константы

---
+++ src/constants/links.ts
@@ -0,0 +1,23 @@
+export const links = [
+  {
+    title: "О нас",
+    href: "about",
+  },
+  {
+    title: "Досуг и развлечения",
+    href: "entertainment",
+  },
+  {
+    title: "Палатки",
+    href: "tents",
+  },
+  {
+    title: "Галерея",
+    href: "gallery",
+  },
+
+  {
+    title: "Правила",
+    href: "rules",
+  },
+];
